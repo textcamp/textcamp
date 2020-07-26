710f03e2c53cb717f2d82096c8beff8a195a2ca3
Peat Bakke <peat@peat.org>
2020-07-26 15:41:57 -0700

    Note about using a comprehensive method for detecting credentials, but it's async so ... more plumbing required.

--- src/services/mod.rs
+++ src/services/mod.rs
@@ -6,6 +6,7 @@ use rusoto_core::credential::*;
 
 /// Checks to see if there is an AWS region specified in the environment or configuration
 pub fn service_credentials() -> bool {
+    // TODO: Convert this to ChainProvider::new().credentials().is_ok() ... which is async, sigh.
     match std::env::var("AWS_DEFAULT_REGION").or_else(|_| std::env::var("AWS_REGION")) {
         Ok(_) => true,
         Err(_) => match ProfileProvider::region() {
